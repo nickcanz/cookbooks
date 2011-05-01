b6be030706389260681b840e790130005829307a
Nicholas Canzoneri <ac3522@gmail.com>
2011-04-30 21:23:37 -0400

    Changing how to install dotfiles again

--- my-config-files/recipes/default.rb
+++ my-config-files/recipes/default.rb
@@ -1,7 +1,9 @@
-deploy "~/.dotfiles" do
-    repo "git://github.com/ac3522/dotfiles.git"
-    revision "HEAD"
-    action :deploy
-    migrate true
-    migration_command "rake install"
+git "~/.dotfiles" do
+    repository "git://github.com/ac3522/dotfiles.git"
+    action :sync
+end
+
+execute "rake_install" do
+  cwd "~/.dotfiles"
+  command "rake install"
 end
