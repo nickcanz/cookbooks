f0b6d74dec029474253cb1821106a5153a009528
Nicholas Canzoneri <ac3522@gmail.com>
2011-08-16 00:30:32 -0400

    Addeding /usr/lib/local to /etc/ld.so.conf so that ldconfig will work when installing geos

--- geos/recipes/default.rb
+++ geos/recipes/default.rb
@@ -15,6 +15,7 @@ end
 bash "build geos from source" do
   cwd "/tmp"
   code <<-EOH
+  sudo awk '{ if (NR == 1) {gsub(/$/, " /usr/lib/local"); 1; print; }}' /etc/ld.so.conf > output.temp && sudo mv output.temp /etc/ld.so.conf
   curl -C - -O http://download.osgeo.org/geos/geos-3.3.0.tar.bz2
   tar xjvf geos-3.3.0.tar.bz2
   cd geos-3.3.0
