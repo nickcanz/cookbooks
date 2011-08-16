bea278bc1a6a40908cfcf9d98f44b196f5d724a8
Nicholas Canzoneri <ac3522@gmail.com>
2011-08-15 23:44:51 -0400

    Cookbook to install curl

---
+++ curl/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: curl
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_package "curl" do
+  action :install
+end
