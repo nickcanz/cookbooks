9f629447763cbc8420277443ecbd217448093b3a
Nicholas Canzoneri <ac3522@gmail.com>
2011-08-17 23:08:55 -0400

    Adding a bunch of packages to opentreemap recipe

--- opentreemap/recipes/default.rb
+++ opentreemap/recipes/default.rb
@@ -12,6 +12,17 @@ include_recipe "apache2"
 include_recipe "python"
 include_recipe "postgresql::server"
 include_recipe "django"
+include_recipe "postgis"
+
+%w{ libapache2-mod-wsgi python-psycopg2 binutils libgeos-c1 libgdal1-1.6.0 libproj0
+    python-django-tagging python-imaging python-xlrd python-feedparser python-memcache
+    python-beautifulsoup python-django-debug-toolbar python-simplejson
+    python-django-extensions python-gdal
+}.each do |pkg|
+  apt_package pkg do
+    action :install
+  end
+end
 
 git "/home/vagrant/opentreemap" do
   repository "git://github.com/azavea/OpenTreeMap.git"
